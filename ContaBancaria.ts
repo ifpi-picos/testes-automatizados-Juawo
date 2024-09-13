217f90874b0862355cbbf2ad7e8f3c4a74fa2fea
Joao Pedro <joaopedrojp2703@gmail.com>
2024-09-13 18:08:04 -0300

    Inciando classe contaBancaria e alterando Readme

--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,23 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+  private numeroConta = Number(prompt("Digite o número da conta : "))
+  private agencia = Number(prompt("Digite o número da agência : "))
+  private saldo = 0;
+  private extrato : String[] = []
+
+  public depoistar(valor : number){
+    return
+  }
+  public sacar(valor : number){
+    return
+  }
+  public transferir(valor : number, contaDestino : ContaBancaria){
+    return
+  }
+  public consultarSaldo(){
+    return
+  }
+  private registarOperação(descricao : string){
+    return
+  }
+
+}
